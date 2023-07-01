e26d687840d2e156777249a2f957417183b2bd88
Bryan Aguinaldo <bryanaguinaldo.e@gmail.com>
2023-07-01 12:33:39 +0800

    title change

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "That One Game Where We Have To Say Stop",
+    title: "Stop Game | That One Game Where We Have To Say Stop",
     description:
-        "Hey Filipinos! Remember that one game we used to play on paper where we draw stuff like head shapes, hairs, eyes, and others? The one where we'd point at those drawings back and forth and where we say stop is the one we'll be drawn as? This is a virtual version of that game!",
+        "Stop Game: Hey Filipinos! Remember that one game we used to play on paper where we draw stuff like head shapes, hairs, eyes, and others? The one where we'd point at those drawings back and forth and where we say stop is the one we'll be drawn as? This is a virtual version of that game!",
 };
 
 export default function RootLayout({ children }) {
