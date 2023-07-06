ab753c225ec4ffe18803dea8c10459c7a956ebcd
Bryan Aguinaldo <bryanaguinaldo.e@gmail.com>
2023-07-06 21:00:57 +0800

    update GA

--- src/app/components/GoogleAnalytics.js
+++ src/app/components/GoogleAnalytics.js
@@ -4,14 +4,14 @@ import Script from "next/script";
 function GoogleAnalytics() {
     return (
         <>
-            <Script src="https://www.googletagmanager.com/gtag/js?id=G-J9R731N32B" />
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-24EC63YC0N" />
             <Script id="google-analytics">
                 {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-J9R731N32B');
+          gtag('config', 'G-24EC63YC0N');
         `}
             </Script>
         </>
